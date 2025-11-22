986e6df4cdc30e381cbc6813a25255b5d10a8b2c
David Gutiérrez <dvdgp93@gmail.com>
2025-11-22 19:20:10 +0100

    Fix funcionalidad email

--- app/Mail/AppointmentReminder.php
+++ app/Mail/AppointmentReminder.php
@@ -5,15 +5,14 @@ namespace App\Mail;
 use App\Models\Appointment;
 use App\Models\Patient;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class AppointmentReminder extends Mailable implements ShouldQueue
+class AppointmentReminder extends Mailable
 {
-    use Queueable, SerializesModels;
+    use SerializesModels;
 
     /**
      * Create a new message instance.
